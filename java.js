e8180cf0c48f4734f45466b5a6134047a75da243
Aquets <37075016+Aquets@users.noreply.github.com>
2018-07-13 19:18:56 +0200

    ended works part

--- java.js
+++ java.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	affixSelector();
 	scrollSpy();
 	homeScroll();
+	photo_popup();
 });
 
 
@@ -74,4 +75,28 @@ $(".nav_top").on("click",function(){
     }, 500);
 
 	});
+};
+
+function photo_popup(){
+$(".photo_grid_item img").on('click', function() {
+
+	alt="";
+	alt=$(this).attr('alt');
+	$('.photo_back').removeClass("hide");
+	$('.photo_back img[alt='+alt+']').removeClass("hide");
+	$('.photo_back img[alt='+alt+']').addClass("see_it");
+	$('.photo_back').addClass("see_it");
+
+
+});
+$(".photo_back").on('click', function() {
+
+	$('.photo_back').addClass("hide");
+	$('.photo_back img[alt='+alt+']').addClass("hide");
+	$('.photo_back img[alt='+alt+']').removeClass("see_it");
+	$('.photo_back').removeClass("see_it");
+
+
+});
+
 };
